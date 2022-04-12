1971924e3b82d8a8462b01b66d1eb38b403f8ec7
tomtomturbo <thomasreist@gmail.com>
2022-04-12 16:37:10 +0200

    1st commit

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/main.js"></script>
-</head>
-<body>
-    <?php
-        echo "Hello, we are starting to work with Databases and PHP PDO!"; 
-    ?>
-    
+<?php include "header.php";
+
+$query = $dbh->query("SELECT * FROM Questions");
+
+$questions = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($questions as $key => $question) {
+    $subQuery = $dbh->prepare("SELECT * from Answers where Answers.QuestionID = ?");
+    $subQuery->bindValue(1, $question['ID']);
+    $subQuery->execute();
+    $answers = $subQuery->fetchAll(PDO::FETCH_ASSOC);
+
+    $questions[$key]['Answers'] = $answers;
+};
+$firstQuestion = $questions[0];
+
+
+echo '<div class="container-fluid p-5">';
+echo '<h2>' . $firstQuestion['Text'] . '</h2>';
+
+echo '<div class="row border">';
+echo '<div class="col-6">';
+echo 'A:';
+echo '</div>';
+echo '<div class="col-6">';
+echo 'B:';
+echo '</div>';
+echo '</div>';
+
+echo '<div class="row border">';
+echo '<div class="col-6">';
+echo 'C:';
+echo   '</div>';
+echo '<div class="col-6">';
+echo 'D:';
+echo '</div>';
+echo '</div>';
+echo '<br>';
+echo '<button type="submit" class="btn btn-outline-primary btn-sm">Primary</button>'
+
+?>
+
 </body>
+
 </html>
